d8ffd704860d58a46044b5540d2d218a6d99254f
Krissie <kristinalauwers@gmail.com>
2019-12-09 10:14:42 -0500

    Mac app store version
    Not only did this version successfully upload, but it actually has appeared in App Store Connect. May be signed correctly.

--- Packages/buildMacDist.sh
+++ Packages/buildMacDist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Set the app version number here
-export VER=1.5
+# Set the app version number here. Number must also be set in Info.plist
+export VER=1.5.1
 
 echo "**** About to build Hummingbird Firmware Burner version $VER for Mac ****"
 echo
@@ -9,14 +9,14 @@ echo "Removing any old products..."
 
 # Remove old products
 rm -rfv Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
-rm -fv Mac/HummingbirdFirmwareBurner.dmg
+# rm -fv Mac/HummingbirdFirmwareBurner.dmg
 rm -fv Mac/HummingbirdFirmwareBurner.pkg
 
 echo
 echo "**** Making the .app ****"
 
 # Make the .app
-javapackager -deploy -native image -name "Hummingbird Firmware Burner" -Bicon=Mac/supportFiles/HummingbirdRoundOrange.icns -BappVersion=$VER -Bidentifier=com.birdbraintechnologies.HummingbirdFirmwareBurner -srcdir . -srcfiles HummingbirdFirmwareBurner.jar -outfile "Hummingbird Firmware Burner" -outdir Mac/supportFiles -v -appclass HummingbirdFirmwareBurner
+javapackager -deploy -native image -name "Hummingbird Firmware Burner" -Bicon=Mac/supportFiles/HummingbirdRoundOrange.icns -BappVersion=$VER -Bidentifier=com.birdbraintechnologies.HummingbirdFirmwareBurner -Bmac.signing-key-developer-id-app="3rd Party Mac Developer Application: Tom Lauwers" -srcdir . -srcfiles HummingbirdFirmwareBurner.jar -outfile "Hummingbird Firmware Burner" -outdir Mac/supportFiles -v -appclass HummingbirdFirmwareBurner
 
 # Copy extra files needed (that cannot be included automatically because of a bug in javapackager
 /bin/cp  -R  Mac/appFiles/*  Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/.
@@ -25,18 +25,23 @@ javapackager -deploy -native image -name "Hummingbird Firmware Burner" -Bicon=Ma
 
 # Sign the app again because of the changes
 # Check syntax of the entitlements file with 'plutil entitlements.plist'
-codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac
-codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper
-codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime
-codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/
+#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac
+#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper
+#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime
+#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/
+codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac -vvvv
+codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper -vvvv
+codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime -vvvv
+codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/ -vvvv
 
 
-echo
-echo "**** Making the .dmg ****"
+# echo
+# echo "**** Making the .dmg ****"
 
 # Make a .dmg for manual distribution
-appdmg Mac/supportFiles/appdmg.json Mac/HummingbirdFirmwareBurner.dmg
-codesign -f -s "Developer ID Application: Tom Lauwers" Mac/HummingbirdFirmwareBurner.dmg
+# Need to sign the app with the other certificate?
+# appdmg Mac/supportFiles/appdmg.json Mac/HummingbirdFirmwareBurner.dmg
+# codesign -f -s "Developer ID Application: Tom Lauwers" Mac/HummingbirdFirmwareBurner.dmg
 
 echo
 echo "**** Making the .pkg ****"
@@ -52,7 +57,7 @@ echo
 echo "**** Checking signatures..."
 echo "... for the .app"
 spctl -a -t exec -vv Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
-echo "... for the .dmg"
-spctl -a -t install -vv Mac/HummingbirdFirmwareBurner.dmg
+# echo "... for the .dmg"
+# spctl -a -t install -vv Mac/HummingbirdFirmwareBurner.dmg
 echo "... for the .pkg"
 pkgutil --check-signature Mac/HummingbirdFirmwareBurner.pkg
