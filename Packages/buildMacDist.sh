10383223a4b73f98a9583bf9f3e52d90ae22c6a4
KristinaLauwers <kristinalauwers@gmail.com>
2021-10-06 12:55:37 -0400

    Update mac packaging
    Fix the script to prepare a package for mac notarization. Correct entitlements for new requirements. Remove some unused files.

--- Packages/buildMacDist.sh
+++ Packages/buildMacDist.sh
@@ -9,7 +9,6 @@ echo "Removing any old products..."
 
 # Remove old products
 rm -rfv Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
-# rm -fv Mac/HummingbirdFirmwareBurner.dmg
 rm -fv Mac/HummingbirdFirmwareBurner.pkg
 
 echo
@@ -25,23 +24,11 @@ javapackager -deploy -native image -name "Hummingbird Firmware Burner" -Bicon=Ma
 
 # Sign the app again because of the changes
 # Check syntax of the entitlements file with 'plutil entitlements.plist'
-#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac
-#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper
-#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime
-#codesign -f -s "Developer ID Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/
 codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac -vvvv
 codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper -vvvv
 codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime -vvvv
 codesign -f -s "3rd Party Mac Developer Application: Tom Lauwers" --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/ -vvvv
 
-
-# echo
-# echo "**** Making the .dmg ****"
-
-# Make a .dmg for manual distribution
-# Need to sign the app with the other certificate?
-# appdmg Mac/supportFiles/appdmg.json Mac/HummingbirdFirmwareBurner.dmg
-# codesign -f -s "Developer ID Application: Tom Lauwers" Mac/HummingbirdFirmwareBurner.dmg
 
 echo
 echo "**** Making the .pkg ****"
@@ -57,7 +44,5 @@ echo
 echo "**** Checking signatures..."
 echo "... for the .app"
 spctl -a -t exec -vv Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
-# echo "... for the .dmg"
-# spctl -a -t install -vv Mac/HummingbirdFirmwareBurner.dmg
 echo "... for the .pkg"
 pkgutil --check-signature Mac/HummingbirdFirmwareBurner.pkg
--- Packages/buildMacDistForNotarization.sh
+++ Packages/buildMacDistForNotarization.sh
@@ -3,33 +3,59 @@
 # Build a disk image for Apple Notarization.
 
 # Set the app version number here. Number must also be set in Info.plist
-export VER=1.5.2
+export VER=1.5.3
+#export JAVAHOME=$(/usr/libexec/java_home)
+#export SRCDIR=Mac/temp
 
-echo "**** About to build Hummingbird Firmware Burner version $VER for Mac Testing ****"
+echo "**** About to build Hummingbird Firmware Burner version $VER for Mac distribution ****"
 echo
-echo "Removing any old products..."
-
-# Remove old products
-rm -rfv Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
+echo "**** Removing old dmg..."
 rm -fv Mac/HummingbirdFirmwareBurner.dmg
 
 echo
 echo "**** Making the .app ****"
+#mkdir $SRCDIR
+#/bin/cp ../out/artifacts/HummingbirdFirmwareBurner_jar/HummingbirdFirmwareBurner.jar $SRCDIR/.
+#/bin/cp  -R  Mac/appFiles/*  $SRCDIR/.
+/bin/cp ../out/artifacts/HummingbirdFirmwareBurner_jar/HummingbirdFirmwareBurner.jar .
 
 # Make the .app
 javapackager -deploy -native image -name "Hummingbird Firmware Burner" -Bicon=Mac/supportFiles/HummingbirdRoundOrange.icns -BappVersion=$VER -Bidentifier=com.birdbraintechnologies.HummingbirdFirmwareBurner -srcdir . -srcfiles HummingbirdFirmwareBurner.jar -outfile "Hummingbird Firmware Burner" -outdir Mac/supportFiles -v -appclass HummingbirdFirmwareBurner
+# javapackager is no longer available in current versions of java. jpackage is
+# the replacement. Below is a command for jpackage. However, Hummingbird Firmware
+# Burner has trouble finding avrdude_mac (no matter where it is placed) when
+# packaged this way... Something may  need to change inside the app to make this
+# work correctly. For now, switch to java 9 in terminal with
+# /usr/libexec/java_home -V
+# export JAVA_HOME=`/usr/libexec/java_home -v 9`
+# java -version
+
+# $JAVAHOME/bin/jpackage \
+#   --type app-image \
+#   --app-version $VER \
+#   --icon Mac/supportFiles/HummingbirdRoundOrange.icns \
+#   --copyright "BirdBrain Technologies LLC" \
+#   --description "Hummingbird Firmware Burner from BirdBrain Technologies" \
+#   --name "Hummingbird Firmware Burner" \
+#   --mac-package-identifier com.birdbraintechnologies.HummingbirdFirmwareBurner \
+#   --vendor "BirdBrain Technologies" \
+#   --input $SRCDIR \
+#   --main-jar HummingbirdFirmwareBurner.jar \
+#   --verbose \
+#   --main-class HummingbirdFirmwareBurner \
+#   --dest Mac/supportFiles
 
 # Copy extra files needed (that cannot be included automatically because of a bug in javapackager
 /bin/cp  -R  Mac/appFiles/*  Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/.
-/bin/cp  -R  sharedResources/*  Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/.
 /bin/cp Mac/supportFiles/Info.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/.
 
 # Sign the app again because of the changes
 # Check syntax of the entitlements file with 'plutil entitlements.plist'
-codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac -vvvv
-codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper -vvvv
-codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --entitlements Mac/supportFiles/entitlementsInherit.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime -vvvv
-codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/ -vvvv
+codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --options runtime --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/Java/avrdude_mac -vvvv
+codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --options runtime --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime/Contents/Home/lib/jspawnhelper -vvvv
+codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --options runtime --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/PlugIns/Java.runtime -vvvv
+#codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/Contents/runtime/Contents/MacOS/libjli.dylib -vvvv
+codesign -f -s "Developer ID Application: Tom Lauwers" --timestamp --options runtime --entitlements Mac/supportFiles/entitlements.plist Mac/supportFiles/Hummingbird\ Firmware\ Burner.app/ -vvvv
 
 
 echo
@@ -44,7 +70,16 @@ echo "**** Checking signatures..."
 echo "... for the .app"
 spctl -a -t exec -vv Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
 echo "... for the .dmg"
+# The dmg signature will now only be accepted after notarization
 spctl -a -t install -vv Mac/HummingbirdFirmwareBurner.dmg
+
+echo
+echo "**** Cleaning up..."
+rm -rf Mac/supportFiles/Hummingbird\ Firmware\ Burner.app
+#rm -rf $SRCDIR
+rm HummingbirdFirmwareBurner.jar
+echo "DONE"
+
 
 # Notarization process (see https://developer.apple.com/documentation/xcode/notarizing_macos_software_before_distribution/customizing_the_notarization_workflow)
 # When you are ready to upload for notification run (only if you have multiple versions of xcode):
@@ -55,7 +90,7 @@ spctl -a -t install -vv Mac/HummingbirdFirmwareBurner.dmg
 #
 # You will receive an email when the process is finished. Also check with
 # xcrun altool --notarization-history 0 -u kristinalauwers@gmail.com -p "@keychain:AC_PASSWORD"
-# Get details with (where "Request_UUID" is the actual Request UUID 
+# Get details with (where "Request_UUID" is the actual Request UUID
 # xcrun altool --notarization-info "Request_UUID" -u kristinalauwers@gmail.com -p "@keychain:AC_PASSWORD"
 #
 # Make sure to read the log file.
